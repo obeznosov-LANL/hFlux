270ca7b310f0f8490bfd310f1d82647d7bbad13c
Oleksii Beznosov <obeznosov@lanl.gov>
2026-06-30 21:13:56 -0600

    Changed hermite data layout

--- include/hFlux/FieldData3D.hpp
+++ include/hFlux/FieldData3D.hpp
@@ -4,6 +4,16 @@
 #include "common.hpp"
 #include "StructuredLocator.hpp"
 
+
+// Hermite coefficients layout for best point evaluation:
+// hermite_data(idR, idZ, packed_component, iR, iZ)
+// packed_component = ndims * fueier_channel + quantity
+// quantity 0: R B_R
+// quantity 1: R B_phi
+// quantity 2: R B_Z from 2D cleaning on each fouier channel
+// quantity 3: R B_Z correction for phi dependency
+
+
 template<int m, int swidth,  typename ExecSpace, int ndims=3>
 struct FieldData3D {
   using DataView =
@@ -28,13 +38,28 @@ struct FieldData3D {
       : fd_locator(R0, Z0, dR, dZ, nR_data - 1, nZ_data - 1),
         nphi(nphi_data), dphi(dphi),
         hermite_locator(makeHermiteLocator<swidth>(fd_locator)),
-        data("data", nR_data, nZ_data, ndims * nPhi_data),
+        data("data", nR_data, nZ_data, ndims * nphi_data),
         hermite_data("hermite_data",
-                      2 * m + 3, 2 * m + 3, ndims * nPhi_data + nPhi_data,   /// To additionally store BZ correction
+                      2 * m + 3, 2 * m + 3, (ndims+1) * nphi_data,
                       hermite_locator.nR,
                       hermite_locator.nZ),
         psi_data("psi_data",
                   2 * m + 3, 2 * m + 3,
                   hermite_locator.nR,
                   hermite_locator.nZ) {}
+
+  KOKKOS_INLINE_FUNCTION
+  static int sample_component(int iphi, int d) {
+    return ndims * iphi + d;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  static int fourier_component(int channel, int d) {
+    return (ndims+1) * channel + d;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  static int correction_component(int channel) {
+    return (ndims+1) * channel + ndims;
+  }
 };
