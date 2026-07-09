4279da8d18b286f76ff31094a1fde0a24e6a2bbb
Oleksii Beznosov <obeznosov@lanl.gov>
2026-07-09 13:52:12 -0600

    removed dead code

--- src/hFlux.cpp
+++ src/hFlux.cpp
@@ -1,26 +1,3 @@
 #include <iostream>
 #include "hFlux/hFlux.hpp"
 
-using namespace std;
-extern "C" {
-    void initialize() {
-    }
-
-    void setRawFieldData() {
-
-    }
-
-    void interpolate() {
-
-    }
-
-    void evaluate() {
-
-    }
-
-    void plotPoincareSection() {
-
-    }
-
-
-}
